edd06801d5a69d580d68e4d407ea8de111fee224
utopfish <utopfish@163.com>
2019-11-12 21:14:05 +0800

    想添加多线程，消耗反而更大，试试消费者生产者模式来解决

--- test.py
+++ test.py
@@ -3,13 +3,82 @@
 #@contact:utopfish@163.com
 #@file:test.py
 #@time: 2019/10/17 22:44
+from multiprocessing import Process,Pool
+from singleCharacterFitch import *
+import threading
+
+class RetThread(threading.Thread):
+
+    def __init__(self, target=None, name=None, args=(), \
+                 kwargs={}, createChild=True, daemon=None):
+        threading.Thread.__init__(self, group=None, target=target, \
+                                  name=name, args=args, kwargs=kwargs, daemon=daemon)
+        self.__ret = None
+        self.__stop = True
+        self.__createChild = createChild
+        self.__guard = None
+
+    def run(self):
+        self.__stop = True
+        if (self.__createChild):
+            guard = RetThread(target=self._target, args=self._args, \
+                              kwargs=self._kwargs, createChild=False, daemon=True)
+            self.__guard = guard
+            self.__guard.start()
+            while (self.__stop and self.__guard.is_alive()):
+                pass
+            del self._target, self._args, self._kwargs
+        else:
+            try:
+                if self._target:
+                    self.__ret = self._target(*self._args, **self._kwargs)
+            finally:
+                del self._target, self._args, self._kwargs
+
+    def getReturn(self):
+        if (self.__guard != None and self.__ret == None):
+            self.__ret = self.__guard.getReturn()
+        return self.__ret
+
+    def stop(self):
+        if not self._initialized:
+            raise RuntimeError(self.getName() + " Thread.__init__() not called")
+        if not self._started.is_set():
+            raise RuntimeError("cannot stop thread until it is started")
+        self.__stop = False
 
 
 
-import tensorflow as tf
-hello = tf.constant('Hello, TensorFlow!')
-sess = tf.Session()
-print(sess.run(hello))
 
+if __name__ == '__main__':
+    path = r"testData\011号简化数据集奇虾\011号完整数据集.txt"
+    data = readDataTxt(path)
+    li = np.array(data)
 
+    te = ["((0,1),(2,(3,4)))", "({0,1,2,3},4)"]
+    start=time.time()
+    print("{}:{}".format(te[0], getFict(te[0], li)))
+    print(time.time()-start)
+    record=[]
+    # start=time.time()
+    # for i in  range(1,len(li[0])):
+    #     process = Process(target=getSingleChararcterFitch, args=(te[0],li[:,i],))
+    #     process.start()
+    #     record.append(process)
+    # for process in record:
+    #     process.join()
+    # print(time.time()-start)
+    start=time.time()
+    for i in range(1,len(li[0])):
+        t = RetThread(target=getSingleChararcterFitch, args=(te[0],li[:,i],))
+        record.append(t)
+    for i in record:   # start threads 此处并不会执行线程，而是将任务分发到每个线程，同步线程。等同步完成后再开始执行start方法
+        i.start()
+        i.join()
+    res=[]
+    p=Pool(processes=4)
+    for i in range(1,len(li[0])):
+        r=p.apply(getSingleChararcterFitch,(te[0],li[:,i],))
+        res.append(r)
+    print(time.time()-start)
 
